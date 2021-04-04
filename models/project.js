f070838596c49f1b5d27906105ade5968a46e477
Hafeezul Kareem Shaik <hafeezulkareem@gmail.com>
2021-04-04 20:05:54 +0530

    Add time stamp for projects

--- models/project.js
+++ models/project.js
@@ -1,24 +1,27 @@
 const mongoose = require("mongoose");
 
-const projectSchema = mongoose.Schema({
-   title: {
-      type: String,
-      maxlength: 256,
-      trim: true,
+const projectSchema = mongoose.Schema(
+   {
+      title: {
+         type: String,
+         maxlength: 256,
+         trim: true,
+      },
+      description: {
+         type: String,
+         maxlength: 500,
+         trim: true,
+      },
+      admin: {
+         type: mongoose.Schema.ObjectId,
+         ref: "User",
+      },
+      stages: {
+         type: Array,
+         default: [],
+      },
    },
-   description: {
-      type: String,
-      maxlength: 500,
-      trim: true,
-   },
-   admin: {
-      type: mongoose.Schema.ObjectId,
-      ref: "User",
-   },
-   stages: {
-      type: Array,
-      default: [],
-   },
-});
+   { timestamp: true }
+);
 
 module.exports = mongoose.model("Project", projectSchema);
